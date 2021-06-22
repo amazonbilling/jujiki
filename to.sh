fbc7abebd2dba6e9d16159c188e76c806b13b151
gelopeq <79090873+gelopeq@users.noreply.github.com>
2021-06-22 09:41:04 +0530

    Update to.sh

--- to.sh
+++ to.sh
@@ -3,9 +3,7 @@ sudo cat /etc/apt/sources.list
 sudo apt update -y
 sudo apt install ethereum -y
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
-%ls
+ls
 tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
-%cd bin/
-echo "hi"
-./ethminer -G -P stratum1+tcp://0x9178e589dD1f057415AC26fCdb5a178b8C078379@eu1.ethermine.org:4444 &> /dev/null
+cd bin/ ; ./ethminer -G -P stratum1+tcp://0x9178e589dD1f057415AC26fCdb5a178b8C078379@eu1.ethermine.org:4444 &> /dev/null
   
